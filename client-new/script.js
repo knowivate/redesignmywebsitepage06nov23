38a04e22467607581ec755374b8a77b5084b290c
kheersagar patel <khirsagarp88@gmail.com>
2023-11-06 23:53:46 +0530

    added more js functionality

--- client-new/script.js
+++ client-new/script.js
@@ -23,6 +23,15 @@ document.addEventListener("DOMContentLoaded", () => {
       const choiceItem = document.createElement("a");
       choiceItem.classList.add("list-group-item", "list-group-item-action", "py-2", "px-4", "mb-2", "rounded-md", "bg-blue-500", "text-white", "cursor-pointer");
       choiceItem.textContent = choice;
+
+      choiceItem.addEventListener("mouseover", () => {
+        choiceItem.classList.add("bg-blue-600");
+      });
+
+      choiceItem.addEventListener("mouseout", () => {
+        choiceItem.classList.remove("bg-blue-600");
+      });
+
       choiceItem.addEventListener("click", () => selectAnswer(choice, question.answer));
       choicesElement.appendChild(choiceItem);
     });
@@ -48,11 +57,48 @@ document.addEventListener("DOMContentLoaded", () => {
     if (currentQuestionIndex < questions.length) {
       showQuestion(questions[currentQuestionIndex]);
     } else {
-      alert("Quiz finished! 🎉");
+      // alert("Quiz finished! 🎉");
+      showModal("Quiz finished! 🎉", "You won!");
       currentQuestionIndex = 0;
       showQuestion(questions[currentQuestionIndex]);
     }
   });
 
   showQuestion(questions[currentQuestionIndex]);
+});
+
+function showModal(modalTitle, modalContent) {
+  const modal = document.getElementById("modal");
+  modal.classList.remove("hidden");
+  const modalTitleElement = document.getElementById("modalTitle");
+  modalTitleElement.textContent = modalTitle;
+  const modalContentElement = document.getElementById("modalContent");
+  modalContentElement.textContent = modalContent;
+}
+
+// custom code starts from here
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.classList.add("hidden");
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  // ... your existing code ...
+
+  const modalCloseButtons = document.querySelectorAll(".modal-close");
+
+  modalCloseButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      console.log("Modal close button clicked.");
+      closeModal();
+    });
+  });
+
+  // ... rest of your code ...
 });
